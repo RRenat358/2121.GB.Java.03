f0472171cd7d4e9a8f02e1bac0f093b504275640
rrenat358 <rrenat358@gmail.com>
2022-08-16 16:59:45 +0300

    1659

---
+++ Lesson01/src/ArrayChangeElement/Generic_ArrayChange.java
@@ -0,0 +1,24 @@
+package ArrayChangeElement;
+
+
+
+
+
+public class Generic_ArrayChange <T>{
+
+    private T obj;
+
+    public Generic_ArrayChange(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+
+
+
+
+
+}
