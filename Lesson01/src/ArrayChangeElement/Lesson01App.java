3e9b26ce1b40370e111d6edc0cbd18ec15ad1687
rrenat358 <rrenat358@gmail.com>
2022-08-18 12:44:52 +0300

    1244

--- Lesson01/src/ArrayChangeElement/Lesson01App.java
+++ Lesson01/src/ArrayChangeElement/Lesson01App.java
@@ -9,7 +9,9 @@ public class Lesson01App {
         System.out.println("\n––– Array for ArrayChangeElement –––––––––––––––––––––––––––\n");
         //Ввод данных здесь
         Integer[] arrInt = {-2, -1, 0, 1, 2, 3, 4, 5, 6}; //arrLength = 9
-        String[] arrStr = {"a", "b", "c", "d", "e", "f", "g"}; //arrLength = 7
+        Double[] arrDb = {1.1, 2.2, 3.3, 4.4};
+        Character[] arrChr = {'П', 'Р', 'И', 'В', 'Е', 'Т'};
+        String[] arrStr = {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg"}; //arrLength = 7
 
         int changeElement_1 = 2;
         int changeElement_2 = 5;
@@ -20,25 +22,23 @@ public class Lesson01App {
         ArrayChangeElementGeneric<Integer> arrGenInt = new ArrayChangeElementGeneric<>(arrInt);
         System.out.println(arrGenInt.toString());
         arrGenInt.showArr(arrInt);
+        arrChangEl3(arrInt);
+
 
         System.out.println("\n--------------------------------------------------\n");
         ArrayChangeElementGeneric<String> arrGenStr = new ArrayChangeElementGeneric<>(arrStr);
         System.out.println(arrGenStr.toString());
         arrGenStr.showArr(arrStr);
-
-
-
+        arrChangEl3(arrStr);
 
 
     }
 
-    /*
-    public void showArr2(T... another) {
-        System.out.println(Arrays.toString(another));
 
+    public static <T> double arrChangEl3(T[] array) {
+        System.out.println(Arrays.toString(array));
+        return 0;
     }
 
-
-     */
 
 }
