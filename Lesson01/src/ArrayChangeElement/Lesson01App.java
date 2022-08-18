a337c73054db118b489381939ce9d9a0efae94f3
rrenat358 <rrenat358@gmail.com>
2022-08-18 14:10:58 +0300

    1410

--- Lesson01/src/ArrayChangeElement/Lesson01App.java
+++ Lesson01/src/ArrayChangeElement/Lesson01App.java
@@ -6,19 +6,18 @@ import java.util.Arrays;
 public class Lesson01App {
     public static void main(String[] args) {
 
-        System.out.println("\n––– Array for ArrayChangeElement –––––––––––––––––––––––––––\n");
+        //======================================================================
+        System.out.println("\n––– Task1. ArrayChangeElement –––––––––––––––––––––––––––");
         //Ввод данных здесь
         Integer[] arrInt = {-2, -1, 0, 1, 2, 3, 4, 5, 6}; //arrLength = 9
         Double[] arrDb = {1.1, 2.2, 3.3, 4.4, 5.5};
         Character[] arrChr = {'П', 'Р', 'И', 'В', 'Е', 'Т', '!'};
         String[] arrStr = {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg"}; //arrLength = 7
 
-        int changeElement_1 = 2;
-        int changeElement_2 = 5;
+        int changePosElement_1 = 2;
+        int changePosElement_2 = 5;
 
-        //======================================================================
-        System.out.println("\n––– ArrayChangeElement –––––––––––––––––––––––––––");
-        System.out.println("\n––– Вариант01. КлассДжинерик –––––––––––––––––––––––––––\n");
+        System.out.println("\n––– Вариант01. КлассДжинерик –––\n");
         ArrayChangeElementGeneric<Integer> arrIntGen = new ArrayChangeElementGeneric<>(arrInt);
         System.out.println(arrIntGen.toString());
         arrIntGen.arrChangGen(arrInt);
@@ -28,18 +27,26 @@ public class Lesson01App {
         System.out.println(arrStrGen.toString());
         arrStrGen.arrChangGen(arrStr);
 
-        System.out.println("\n––– Вариант01. КлассДжинерик –––––––––––––––––––––––––––\n");
-        arrChang(arrInt);
-        arrChang(arrStr);
+        System.out.println("\n––– Вариант02. МетодДжинерик –––\n");
+//        arrChang(arrInt);
+//        arrChang(arrStr);
+        arrChang(arrStr, changePosElement_1, changePosElement_1);
 
 
     }
 
+/*
 
     public static <T> double arrChang(T[] array) {
         System.out.println(Arrays.toString(array));
         return 0;
     }
+*/
 
+    public static <T> double arrChang(T[] array, int changePosElement_1, int changePosElement_2) {
+        System.out.println(Arrays.toString(array));
+        System.out.println(changePosElement_1 + changePosElement_2 + 50);
+        return 0;
+    }
 
 }
