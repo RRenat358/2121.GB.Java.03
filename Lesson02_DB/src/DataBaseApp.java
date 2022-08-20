bf535c01cd982e74d08df93a63732a15eba8c2b9
rrenat358 <rrenat358@gmail.com>
2022-08-20 11:37:34 +0300

    1137

--- Lesson02_DB/src/DataBaseApp.java
+++ Lesson02_DB/src/DataBaseApp.java
@@ -1,23 +1,48 @@
 import java.sql.Connection;
-
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class DataBaseApp {
 
     private static Connection connection;
+    public static Statement statement;
 
     public static void main(String[] args) {
         try {
             connect();
-
         } catch (Exception ex) {
             ex.printStackTrace();
-
         } finally {
             disconnect();
         }
     }
 
+    private static void connect() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:javadb.db");
+        statement = connection.createStatement();
+    }
+
+
+    private static void disconnect() {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
 
 
 }
