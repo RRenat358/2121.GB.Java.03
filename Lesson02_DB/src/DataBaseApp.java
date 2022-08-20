07a9e23f987e3da09305de60ec0b11bb97a4106c
rrenat358 <rrenat358@gmail.com>
2022-08-20 11:16:37 +0300

    1116

--- Lesson02_DB/src/DataBaseApp.java
+++ Lesson02_DB/src/DataBaseApp.java
@@ -1,9 +1,22 @@
+import java.sql.Connection;
 
 
 
 public class DataBaseApp {
 
+    private static Connection connection;
 
+    public static void main(String[] args) {
+        try {
+            connect();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        } finally {
+            disconnect();
+        }
+    }
 
 
 
