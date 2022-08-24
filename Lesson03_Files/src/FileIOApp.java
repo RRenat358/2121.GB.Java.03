1a97e9c035ee2b347948e3312ec699ef1aa92e2e
rrenat358 <rrenat358@gmail.com>
2022-08-24 15:37:23 +0300

    1437

--- Lesson03_Files/src/FileIOApp.java
+++ Lesson03_Files/src/FileIOApp.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class FileIOApp {
@@ -14,13 +15,32 @@ public class FileIOApp {
 
             fileOutputStream.write("Java Lava".getBytes());
 
+            long start = System.currentTimeMillis();
             for (int i = 0; i < 1000; i++) {
-
+                fileOutputStream.write(65);
             }
+            System.out.println(System.currentTimeMillis() - start);
+
+            // VS
+
+            byte[] arr = new byte[1000];
+            Arrays.fill(arr, (byte) 65);
+            start = System.currentTimeMillis();
+            fileOutputStream.write(arr);
+            System.out.println(System.currentTimeMillis() - start);
+
+
+            //очистка файла
+//            new FileOutputStream("dir01/demo2.txt").close();
+            new FileOutputStream(file).close();
+
+
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
+
 
 
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
