debe5b0d4b41908cd7447fd80932b3029a8e35e8
rrenat358 <rrenat358@gmail.com>
2022-08-25 14:43:00 +0300

    1442

--- Lesson03_Files/src/WriterApp.java
+++ Lesson03_Files/src/WriterApp.java
@@ -2,6 +2,8 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class WriterApp {
 
@@ -9,9 +11,16 @@ public class WriterApp {
 
         String demo4 = "dir01/demo4.txt";
 
+        String localDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern(
+                "yyyy.MM.dd-HHmmss"));
+
         try (Writer writer = new BufferedWriter(new FileWriter(demo4))) {
 
-            writer.write("LoveIsLove\n");
+            writer.write("=== Start DataUser ==============================\n");
+            writer.write(localDateTime + "\n");
+            // OR
+            writer.append("=== Start DataUser ==============================\n");
+            writer.append(localDateTime + "\n");
 
         } catch (IOException e) {
             e.printStackTrace();
