f63b2a816e127e17118f3f6eaf2d52bcac6cf298
rrenat358 <rrenat358@gmail.com>
2022-08-26 15:01:52 +0300

    1501

--- Lesson03_Files/src/FileReadLastLine.java
+++ Lesson03_Files/src/FileReadLastLine.java
@@ -5,10 +5,25 @@
 
 public class FileReadLastLine {
 
+    String pathFile = "dir01/bigFile-refactoring.txt";
+
+
     public static void main(String[] args) {
 
 
+
+
     }
+
+
+    public void fileReadLastLines() {
+
+
+
+
+    }
+
+
 
 
 
