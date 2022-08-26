ba3289ed8cde90e024f9b3da2a5d1c5ad04a8735
rrenat358 <rrenat358@gmail.com>
2022-08-26 15:21:02 +0300

    1520

--- Lesson03_Files/src/FileReadLastLine.java
+++ Lesson03_Files/src/FileReadLastLine.java
@@ -1,22 +1,32 @@
-
-
-
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class FileReadLastLine {
 
-    String pathFile = "dir01/bigFile-refactoring.txt";
 
+    public static void main(String[] args) throws IOException {
+        String pathFile = "dir01/bigFile-refactoring.txt";
+        int nLast = 10;
 
-    public static void main(String[] args) {
-
+        fileReadLastLines(pathFile, nLast);
 
 
 
     }
 
 
-    public void fileReadLastLines() {
+    public static void fileReadLastLines(String pathFile, int nLast) throws IOException {
+
+        RandomAccessFile randomAccessFile = new RandomAccessFile(pathFile, "r");
+        String oneLastLine = randomAccessFile.readLine();
+
+
+        System.out.println("\n");
+        System.out.println(oneLastLine);
+        System.out.println("\n");
+
+
 
 
 
