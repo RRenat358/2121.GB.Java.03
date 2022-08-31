2a08131c9b8ae6be7d7c966f3d500bdc5ec718ed
rrenat358 <rrenat358@gmail.com>
2022-08-31 21:10:24 +0300

    2110

---
+++ Lesson05-Thread-2/src/CyclicApp.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicApp {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+
+        for (int i = 0; i < 5; i++) {
+            int w = i + 1;
+
+            new Thread(() ->{
+                try {
+                    //task
+
+                    System.out.println("Поток " + w + " готовится...");
+                    Thread.sleep(random.nextInt(5) * 1000);
+                    System.out.println("Поток " + w + " готов");
+                    cyclicBarrier.await();
+
+                    System.out.println("Поток " + w + " поехал...");
+                    Thread.sleep(random.nextInt(5) * 1000);
+                    System.out.println("Поток " + w + " доехал");
+                    cyclicBarrier.await();
+
+                } catch (Exception e) {
+                    //ignore
+                    e.printStackTrace();
+
+                }
+
+            }).start();
+        }
+
+
+
+
+    }
+
+
+
+}
