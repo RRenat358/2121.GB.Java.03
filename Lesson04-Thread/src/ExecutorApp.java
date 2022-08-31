98aeb024182f116b87def8166d131b237c5d1e80
rrenat358 <rrenat358@gmail.com>
2022-08-31 18:09:45 +0300

    1809

--- Lesson04-Thread/src/ExecutorApp.java
+++ Lesson04-Thread/src/ExecutorApp.java
@@ -1,15 +1,13 @@
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class ExecutorApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
 
+        //======================================================================
         executorService.execute(new Runnable() {
             @Override
             public void run() {
@@ -21,22 +19,36 @@ public class ExecutorApp {
         executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
         executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
 
+        //======================================================================
         Future<?> future = executorService.submit(() -> System.out.println("future"));
 
 
+        //======================================================================
+        Future<String> futur2 = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "String from Callable -- 01";
+            }
+        });
+        System.out.println(futur2.get());
+
+        Future<String> futur3 = executorService.submit(new MyCallable());
+        System.out.println(futur3.get());
 
 
+
+        //======================================================================
+        //Обязательно завершать
+        executorService.shutdown();
+//        executorService.shutdownNow(); //интреапт всем -- прямо сейчас!
 
     }
 
     private static class MyCallable implements Callable<String> {
         @Override
         public String call() throws Exception {
-            return "String from Callable";
+            return "String from Callable -- 02";
         }
-
-
-
     }
 
 
