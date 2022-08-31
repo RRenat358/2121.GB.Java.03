ba739df404ab7849fa43f5729b9c857f97e3c66b
rrenat358 <rrenat358@gmail.com>
2022-08-31 19:20:32 +0300

    1920

---
+++ Lesson05-Thread-2/src/SemaphoreApp.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreApp {
+
+    public static void main(String[] args) {
+
+        Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < 10; i++) {
+            final int w = i + 1;
+            new Thread(() ->{
+                try {
+                    System.out.println("Поток " + w + " перед семофором...");
+                    semaphore.acquire();
+                    System.out.println("Поток " + w + " получил доступ к ресурсу");
+                    Thread.sleep(500);
+                } catch (Exception exception) {
+
+                } finally {
+                    System.out.println("Поток " + w + " отпустил ресурс");
+                    semaphore.release();
+                }
+
+            }).start();
+        }
+
+    }
+
+
+
+
+}
