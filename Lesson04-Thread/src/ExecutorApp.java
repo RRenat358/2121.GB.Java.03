7acaedfa35099d6c35971625de4cc07a007d845d
rrenat358 <rrenat358@gmail.com>
2022-08-31 17:55:13 +0300

    1755

--- Lesson04-Thread/src/ExecutorApp.java
+++ Lesson04-Thread/src/ExecutorApp.java
@@ -1,11 +1,14 @@
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ExecutorApp {
     public static void main(String[] args) {
 
         ExecutorService executorService = Executors.newCachedThreadPool();
+
 
         executorService.execute(new Runnable() {
             @Override
@@ -13,6 +16,29 @@ public class ExecutorApp {
                 System.out.println(Thread.currentThread().getName());
             }
         });
+        executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+        executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+        executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+        executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+
+        Future<?> future = executorService.submit(() -> System.out.println("future"));
+
+
+
+
+
     }
+
+    private static class MyCallable implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            return "String from Callable";
+        }
+
+
+
+    }
+
+
 
 }
