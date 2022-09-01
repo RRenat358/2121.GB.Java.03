5dc2b28709e42ecf3c29d443a5b74e96b4a690f1
rrenat358 <rrenat358@gmail.com>
2022-09-01 21:19:05 +0300

    2119

--- Lesson04-HW/src/ThreadABC.java
+++ Lesson04-HW/src/ThreadABC.java
@@ -6,7 +6,7 @@ public class ThreadABC {
     private static String A = " A ";
     private static String B = " B ";
     private static String C = " C ";
-    private static String abc = "";
+    private volatile static String abc = "";
 
 
     public static void main(String[] args) {
@@ -16,15 +16,16 @@ public class ThreadABC {
             synchronized (objSync) {
                 try {
                     for (int i = 0; i < 5; i++) {
-                        if (abc != A) {
+                        if (abc == "" || abc == C) {
                             System.out.print(A);
                             abc = A;
+                            objSync.notifyAll();
                         } else {
                             objSync.wait();
                         }
                     }
                 } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    e.printStackTrace();
                 }
             }
         });
@@ -32,16 +33,34 @@ public class ThreadABC {
 
 
         //======================================================================
-        Thread threadB = new Thread(() -> {
+/*        Thread threadB = new Thread(() -> {
             synchronized (objSync) {
                 System.out.print(B);
 
             }
         });
+        threadB.start();*/
+        Thread threadB = new Thread(() -> {
+            synchronized (objSync) {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        if (abc == A) {
+                            System.out.print(B);
+                            abc = B;
+                            objSync.notifyAll();
+                        } else {
+                            objSync.wait();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         threadB.start();
 
-
         //======================================================================
+/*
         Thread threadC = new Thread(() -> {
             synchronized (objSync) {
                 System.out.print(C);
@@ -49,7 +68,25 @@ public class ThreadABC {
             }
         });
         threadC.start();
-
+*/
+        Thread threadC = new Thread(() -> {
+            synchronized (objSync) {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        if (abc == B) {
+                            System.out.print(C);
+                            abc = C;
+                            objSync.notifyAll();
+                        } else {
+                            objSync.wait();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        threadC.start();
 
     }
 
