1f51b8abc407521841f859955ad31da5744ef741
rrenat358 <rrenat358@gmail.com>
2022-09-05 12:38:38 +0300

    1238

--- Lesson05-HW/src/MainClass.java
+++ Lesson05-HW/src/MainClass.java
@@ -1,14 +1,21 @@
-
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class MainClass {
 
     public static final int CARS_COUNT = 4;
 
-    
 
     public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
         Car[] cars = new Car[CARS_COUNT];
 
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
@@ -16,10 +23,24 @@ public class MainClass {
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
+
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+//            new Thread(cars[i]).start();
+            int finalI = i;
+            executorService.execute(() -> {
+                try {
+                    cars[finalI].run();
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+
         }
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 
