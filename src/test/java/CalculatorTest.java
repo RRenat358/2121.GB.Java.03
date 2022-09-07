3a3a410637d9af415db971cda4def15a16f3e6f1
rrenat358 <rrenat358@gmail.com>
2022-09-07 15:04:47 +0300

    1504

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,23 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 public class CalculatorTest {
 
     Calculator calculator1 = new Calculator();
     Calculator calculator2;
+
+
+
+
 
 
     @Test
@@ -24,8 +33,45 @@ public class CalculatorTest {
     }
 
     @Test
+    @Disabled
     void testDivisionWithException() {
         Assertions.assertThrows(ArithmeticException.class, () -> calculator1.division(5, 0));
     }
+
+    @CsvSource({
+            "1, 2, 3",
+            "3, 2, 5",
+            "13, 2, 15"
+    })
+    @ParameterizedTest
+    void testAdditionMultiple(int a, int b, int result) {
+        Assertions.assertEquals(result, calculator1.sum(a, b));
+    }
+
+
+    @MethodSource("dataForAddition")
+    @ParameterizedTest
+    void testAdditionMultipleWithMethod(int a, int b, int result) {
+        Assertions.assertEquals(result, calculator1.sum(a, b));
+    }
+
+    public static Stream<Arguments> dataForAddition() {
+        List<Arguments> argumentsList = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            int a = random.nextInt(1000);
+            int b = random.nextInt(1000);
+            argumentsList.add(Arguments.arguments(a, b, a + b));
+        }
+        return argumentsList.stream();
+    }
+
+
+
+
+
+
+
+
 
 }
