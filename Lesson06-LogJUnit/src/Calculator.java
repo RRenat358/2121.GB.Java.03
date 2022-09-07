837c7ed7cc79eee1805e50633fcbf3f232bc77b7
rrenat358 <rrenat358@gmail.com>
2022-09-07 13:35:06 +0300

    1335

---
+++ Lesson06-LogJUnit/src/Calculator.java
@@ -0,0 +1,35 @@
+
+
+
+public class Calculator {
+
+    private Battery battery;
+
+    public Calculator() {
+        battery = new Battery(100);
+    }
+
+    public Calculator(Battery battery) {
+        this.battery = battery;
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int multiply(int a, int b) {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+
+
+}
