540377f5b06a44176c8c14d9617279a9488e8922
rrenat358 <rrenat358@gmail.com>
2022-09-07 19:54:34 +0300

    1954

---
+++ Lesson06-LogJUnit/src/CurrentClass.java
@@ -0,0 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.Logger;
+
+
+public class CurrentClass {
+    private static final Logger logger = LogManager.getLogger(CurrentClass.class);
+
+
+}
