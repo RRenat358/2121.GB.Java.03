acc4e83927135c5a43ec3a844917d17d890707c4
rrenat358 <rrenat358@gmail.com>
2022-09-07 13:38:33 +0300

    1338

---
+++ Lesson06-LogJUnit/src/AssertApp.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class AssertApp {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        assert a < b;
+
+        System.out.println("ok");
+
+    }
+
+
+}
