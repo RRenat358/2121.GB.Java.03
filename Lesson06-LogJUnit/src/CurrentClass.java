1b3008bbc8877ecc46f4f7db23681750264fbb6f
rrenat358 <rrenat358@gmail.com>
2022-09-08 08:27:05 +0300

    0827

--- Lesson06-LogJUnit/src/CurrentClass.java
+++ Lesson06-LogJUnit/src/CurrentClass.java
@@ -8,5 +8,41 @@ public class CurrentClass {
 
 
 
+    //logLevels
+
+    // Trace > Debug > Info > Warn > Error > Fatal
+    //     1       2      3      4       5       6
+
+
+    public static void main(String[] args) {
+
+        int logLevel = 2;
+
+        int a = 50;
+        if (logLevel <= 2) {
+            System.out.println(System.currentTimeMillis() + " our method: a = " + a);
+        }
+        logger.debug("a = {}", a);
+
+        int b = 10;
+        if (logLevel <= 2) {
+            System.out.println(System.currentTimeMillis() + " our method: b = " + b);
+        }
+        logger.debug("b = {}", b);
+
+        int c = a + b;
+        if (logLevel <= 2) {
+            System.out.println(System.currentTimeMillis() + " our method: c = " + c);
+        }
+        logger.info("c = {} + {} = {}", a, b, c);
+
+
+        //Logger, Appender, Layout
+
+
+
+
+    }
+
 
 }
