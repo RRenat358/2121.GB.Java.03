019e0cf8b76384468177afc3c03e0105d71be56f
rrenat358 <rrenat358@gmail.com>
2022-09-08 17:16:39 +0300

    1716

--- Lesson06-LogJUnit/src/HomeWork/lesson06HW.java
+++ Lesson06-LogJUnit/src/HomeWork/lesson06HW.java
@@ -1,13 +1,30 @@
 package HomeWork;
 
 
+import java.util.Arrays;
 
 public class lesson06HW {
     public static void main(String[] args) {
 
+        int[] arr1 = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+        int[] arr2 = {5, 2, 9, 4, 2, 3, 5, 1, 4};
+        int[] arr3 = {4, 2, 0, 0, 2, 3, 7, 1, 5};
+
+        int a = 4;
 
 
+        System.out.println(arreyAfterFour(arr1, a));
 
+
+    }
+
+    public static int[] arreyAfterFour(int[] arr, int a) {
+        int[] arr2 = null;
+
+        Arrays.asList(arr).indexOf(a);
+
+
+        return arr2;
 
     }
 
