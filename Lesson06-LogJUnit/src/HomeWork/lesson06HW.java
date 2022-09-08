e576969320c4f206b6473a8c84af3b0c2c34e4ba
rrenat358 <rrenat358@gmail.com>
2022-09-08 21:31:32 +0300

    2131 arrayAfterElement = ok

--- Lesson06-LogJUnit/src/HomeWork/lesson06HW.java
+++ Lesson06-LogJUnit/src/HomeWork/lesson06HW.java
@@ -7,52 +7,58 @@ public class lesson06HW {
     public static void main(String[] args) {
 
         int[] arr1 = {1, 2, 4, 4, 2, 3, 4, 1, 7};
-        int[] arr2 = {5, 2, 9, 4, 2, 3, 5, 1, 4};
-        int[] arr3 = {4, 2, 0, 0, 2, 3, 7, 1, 5};
-        int[] arr4 = {1, 1, 1, 1, 1, 1, 1, 1, 1};
-        int[] arr5 = {400, 200, 1000, 1000, 2000, 4, 300, 700, 10000, 500};
+        int[] arr2 = {4, 2, 0, 0, 2, 3, 7, 1, 5};
+        int[] arr3 = {5, 2, 9, 4, 2, 3, 5, 1, 4};
+        int[] arr4 = {400, 200, 1000, 1000, 2000, 4, 300, 700, 10000, 500};
+        int[] arr5 = {1, 1, 1, 1, 1, 1, 1, 1, 1};
 
-        int target = 4;
+        int element1 = 4;
+        int element2 = 1;
 
 
-        System.out.println("==============================");
-        System.out.println(Arrays.toString(arrayAfterElement(arr1, target)));
-        System.out.println(Arrays.toString(arrayAfterElement(arr2, target)));
-        System.out.println(Arrays.toString(arrayAfterElement(arr3, target)));
-        System.out.println(Arrays.toString(arrayAfterElement(arr4, target)));
-        System.out.println(Arrays.toString(arrayAfterElement(arr5, target)));
 
         System.out.println("==============================");
-        System.out.println(isCheckElementInArray(arr1, target));
-        System.out.println(isCheckElementInArray(arr2, target));
-        System.out.println(isCheckElementInArray(arr3, target));
-        System.out.println(isCheckElementInArray(arr4, target));
-        System.out.println(isCheckElementInArray(arr5, target));
+        System.out.println(Arrays.toString(arrayAfterElement(arr1, element1)));
+        System.out.println(Arrays.toString(arrayAfterElement(arr2, element1)));
+        System.out.println(Arrays.toString(arrayAfterElement(arr3, element1)));
+        System.out.println(Arrays.toString(arrayAfterElement(arr4, element1)));
+//        System.out.println(Arrays.toString(arrayAfterElement(arr5, element1)));
+
+        System.out.println("==============================");
+        System.out.println(isCheckElementInArray(arr1, element1, element2));
+        System.out.println(isCheckElementInArray(arr2, element1, element2));
+        System.out.println(isCheckElementInArray(arr3, element1, element2));
+        System.out.println(isCheckElementInArray(arr4, element1, element2));
+        System.out.println(isCheckElementInArray(arr5, element1, element2));
 
 
     }
 
 
-    public static int[] arrayAfterElement(int[] arr, int target) {
-        int[] arr2;
-        int indexLast;
+    public static int[] arrayAfterElement(int[] arr, int element)  throws CheckElementInArrayException{
         for (int i = arr.length-1; i >=0 ; i--) {
-            if (arr[i] == target) {
-                indexLast = i;
-                arr2 = Arrays.copyOfRange(arr, i + 1,arr.length);
-                return arr2;
+            if (arr[i] == element) {
+                return Arrays.copyOfRange(arr, i + 1,arr.length);
             }
         }
-        return null;
+        throw new CheckElementInArrayException("\nRuntimeException = Element [" + element + "] not found in array");
     }
 
-    public static boolean isCheckElementInArray(int[] arr, int target) throws CheckElementInArrayException {
-        for (int element : arr) {
-            if (element == target) {
+    public static boolean isCheckElementInArray(int[] arr, int element1, int element2) throws CheckElementInArrayException {
+        int checkCount1 = 0;
+        int checkCount2 = 0;
+        for (int i : arr) {
+            if (i == element1) {
+                checkCount1++;
+            }
+            if (i == element2) {
+                checkCount2++;
+            }
+            if (checkCount1 > 0 && checkCount2 > 0) {
                 return true;
             }
         }
-        throw new CheckElementInArrayException("\nRuntimeException = Check Element in Array = " + target);
+        return false;
     }
 
     public static class CheckElementInArrayException extends RuntimeException {
