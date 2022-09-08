2ef7f5b0d67c6c364e6d8f05edc79f5478a8cc6f
rrenat358 <rrenat358@gmail.com>
2022-09-08 20:43:18 +0300

    2043 arrayAfterElement

--- Lesson06-LogJUnit/src/HomeWork/lesson06HW.java
+++ Lesson06-LogJUnit/src/HomeWork/lesson06HW.java
@@ -1,11 +1,7 @@
 package HomeWork;
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class lesson06HW {
     public static void main(String[] args) {
@@ -13,70 +9,50 @@ public class lesson06HW {
         int[] arr1 = {1, 2, 4, 4, 2, 3, 4, 1, 7};
         int[] arr2 = {5, 2, 9, 4, 2, 3, 5, 1, 4};
         int[] arr3 = {4, 2, 0, 0, 2, 3, 7, 1, 5};
-        int[] arr4 = {400, 200, 1000, 1000, 2000, 300, 700, 10000, 500};
+        int[] arr4 = {1, 1, 1, 1, 1, 1, 1, 1, 1};
+        int[] arr5 = {400, 200, 1000, 1000, 2000, 4, 300, 700, 10000, 500};
 
-        int a = 4;
+        int target = 4;
 
 
-        System.out.println(Arrays.toString(arreyAfterFour(arr1, a)));
+        System.out.println("==============================");
+        System.out.println(Arrays.toString(arrayAfterElement(arr1, target)));
+        System.out.println(Arrays.toString(arrayAfterElement(arr2, target)));
+        System.out.println(Arrays.toString(arrayAfterElement(arr3, target)));
+        System.out.println(Arrays.toString(arrayAfterElement(arr4, target)));
+        System.out.println(Arrays.toString(arrayAfterElement(arr5, target)));
+
+        System.out.println("==============================");
+        System.out.println(isCheckElementInArray(arr1, target));
+        System.out.println(isCheckElementInArray(arr2, target));
+        System.out.println(isCheckElementInArray(arr3, target));
+        System.out.println(isCheckElementInArray(arr4, target));
+        System.out.println(isCheckElementInArray(arr5, target));
 
 
     }
 
-    public static int[] arreyAfterFour(int[] arr, int a) {
-        int[] arr2 = null;
-/*
 
-//        ArrayList<Integer> arrayList = new ArrayList<>();
-//        ArrayList<?> arrayList = new ArrayList<>();
-//        arrayList.as;
-
-//        List<Integer> list = new ArrayList<>();
-//        System.out.println(Collections.addAll(new ArrayList<>(), arr));
-
-
-//        Collections.reverse(list);
-//        System.out.println(list.get(1));
-//        System.out.println(list.equals((Object)a));
-
-
-//        Collections.addAll(arrayList, arr);
-
-//        int i = Arrays.asList(arr).indexOf(a);
-//        System.out.println(i);
-//        arr2 = Arrays.copyOf(arr, i);
-
-
-/*
-        List<Integer> list11 = Arrays.stream(arr).boxed().collect(Collectors.toList());
-        System.out.println(list11);
-        System.out.println(list11.lastIndexOf(a));
-*/
-
-//        System.out.println(list11.subList(6 + 1, arr.length));
-//        System.out.println(list11.subList(list11.lastIndexOf(a) + 1, arr.length));
-//        for (Object o : list11){
-//            arr2 = (int[]) o;
-//        }
-
-/*
-        Collections.reverse(Collections.singletonList(arr));
-        Collections.reverse(new ArrayList<>(Arrays.stream(arr).boxed().collect(Collectors.toList())));
-        System.out.println(arr.equals(a));
-        System.out.println(Arrays.toString(arr));
-*/
-
-
+    public static int[] arrayAfterElement(int[] arr, int target) {
+        int[] arr2;
+        int indexLast;
         for (int i = arr.length-1; i >=0 ; i--) {
-            int indexLast;
-            if (arr[i] == a) {
+            if (arr[i] == target) {
                 indexLast = i;
-                arr2 = Arrays.copyOfRange(arr, 6 + 1,arr.length);
+                arr2 = Arrays.copyOfRange(arr, i + 1,arr.length);
                 return arr2;
             }
         }
-        return arr2;
+        return null;
+    }
 
+    public static boolean isCheckElementInArray(int[] arr, int target) {
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
