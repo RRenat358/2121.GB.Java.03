01e5f3b3cab050b386c9e15333489583e51bba39
rrenat358 <rrenat358@gmail.com>
2022-09-14 11:55:58 +0300

    1155

--- Lesson07-Annotation/src/Lesson07.java
+++ Lesson07-Annotation/src/Lesson07.java
@@ -3,9 +3,20 @@
 
 
 public class Lesson07 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 
+        //класс Class
+        Cat cat = new Cat("Basik", "red", 5, 8);
 
+        Class<Cat> catClass1 = Cat.class;
+        Class<Cat> catClass2 = (Class<Cat>) cat.getClass();
+        Class<?> catClass3 = Class.forName("java.lang.Byte");
+
+        System.out.println(catClass1.getName());
+        System.out.println(catClass1.getSimpleName());
+
+        System.out.println(catClass2.getName());
+        System.out.println(catClass2.getSimpleName());
 
 
 
