cbdabd8893507374b532e6088aab76a6d36d8646
rrenat358 <rrenat358@gmail.com>
2022-09-18 19:59:27 +0300

    1959

--- Lesson07-HW/src/MethodR.java
+++ Lesson07-HW/src/MethodR.java
@@ -21,15 +21,46 @@ public class MethodR {
 
 
 
-    @TestR
-    private String prnt() {
-        System.out.println("MethodR = body");
+    @TestR // (priority = 5) default
+    private String prnt1() {
+        System.out.println("MethodR = Test" + ", " + "priority = 5 default");
         return null;
     }
 
-    @TestR
-    private int sum(int a, int b) {
-        return a + b;
+    @TestR(priority = 9)
+    private String prnt2() {
+        System.out.println("MethodR = Test" + ", " + "priority = 9");
+        return null;
+    }
+
+    @TestR(priority = 3)
+    private String prnt3() {
+        System.out.println("MethodR = Test" + ", " + "priority = 3");
+        return null;
+    }
+
+    @TestR(priority = 1)
+    private String prnt4() {
+        System.out.println("MethodR = Test" + ", " + "priority = 1");
+        return null;
+    }
+
+    @TestR(priority = 8)
+    private String prnt5() {
+        System.out.println("MethodR = Test" + ", " + "priority = 8");
+        return null;
+    }
+
+    @TestR(priority = 7)
+    private String prnt6() {
+        System.out.println("MethodR = Test" + ", " + "priority = 7");
+        return null;
+    }
+
+    @TestR(priority = 6)
+    private String prnt7() {
+        System.out.println("MethodR = Test" + ", " + "priority = 6");
+        return null;
     }
 
 
